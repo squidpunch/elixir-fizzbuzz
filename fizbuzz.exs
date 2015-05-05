23cc14c0e52589cf2a28627d74deaaa4f7aa1ba6
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-04 20:17:31 -0400

    Using conditionals

--- fizbuzz.exs
+++ fizbuzz.exs
@@ -1,18 +1,15 @@
 defmodule Fizzbuzz do
   def upto(n) do
     Enum.map (1..n), fn(n) ->
-      if rem(n, 3) == 0 and rem(n, 5) == 0 do
-        "Fizbuzz"
-      else
-        if rem(n, 3) == 0 do
+      cond do
+        rem(n, 3) == 0 and rem(n, 5) == 0 ->
+          "Fizbuzz"
+        rem(n, 3) == 0 ->
           "Fizz"
-        else
-          if rem(n, 5) == 0 do
-            "Buzz"
-          else
-            n
-          end
-        end
+        rem(n, 5) == 0 ->
+          "Buzz"
+        true ->
+          n
       end
     end
   end
