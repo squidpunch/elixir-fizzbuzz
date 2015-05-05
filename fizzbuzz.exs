22052c1943bd2af047f0e12580380cd60ab99cf7
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-05 09:35:17 -0400

    Use matchers & private methods

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,18 +1,9 @@
 defmodule Fizzbuzz do
   def upto(n), do: _upto(n, [])
-  defp _upto(0, result), do: result
-  defp _upto(n, result) do
-    answer = cond do
-      rem(n, 3) == 0 and rem(n, 5) == 0 ->
-        "Fizbuzz"
-      rem(n, 3) == 0 ->
-        "Fizz"
-      rem(n, 5) == 0 ->
-        "Buzz"
-      true ->
-        n
-    end
 
-    _upto(n - 1, [answer|result])
-  end
+  defp _upto(0, result), do: result
+  defp _upto(n, result) when rem(n, 3) == 0 and rem(n, 5) == 0, do: _upto(n - 1, ["FizzBuzz"|result])
+  defp _upto(n, result) when rem(n, 3) == 0, do: _upto(n - 1, ["Fizz"|result])
+  defp _upto(n, result) when rem(n, 5) == 0, do: _upto(n - 1, ["Buzz"|result])
+  defp _upto(n, result), do: _upto(n-1, [n|result])
 end
