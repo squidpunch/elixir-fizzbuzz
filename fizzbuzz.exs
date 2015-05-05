9157ba32ded1b89c6998fe25c92c5c0d5dfbfe38
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-04 20:34:30 -0400

    Private methods - recursion

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,16 +1,18 @@
 defmodule Fizzbuzz do
-  def upto(n) do
-    Enum.map (1..n), fn(n) ->
-      cond do
-        rem(n, 3) == 0 and rem(n, 5) == 0 ->
-          "Fizbuzz"
-        rem(n, 3) == 0 ->
-          "Fizz"
-        rem(n, 5) == 0 ->
-          "Buzz"
-        true ->
-          n
-      end
+  def upto(n), do: _upto(n, [])
+  defp _upto(0, result), do: result
+  defp _upto(n, result) do
+    answer = cond do
+      rem(n, 3) == 0 and rem(n, 5) == 0 ->
+        "Fizbuzz"
+      rem(n, 3) == 0 ->
+        "Fizz"
+      rem(n, 5) == 0 ->
+        "Buzz"
+      true ->
+        n
     end
+
+    _upto(n - 1, [answer|result])
   end
 end
